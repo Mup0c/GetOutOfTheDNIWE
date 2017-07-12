22fdf4588870af0fc3b3207685183aab87ce7d00
LuckThemAll <luckthemall@gmail.com>
2017-07-12 11:47:57 +1000

    consts add

---
+++ LevelGeneration.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const FIELD_WIDTH = 18;
+const FIELD_HEIGHT = 12;
+const WALL_WIDTH = 3;
+const EMPTY_BLOCK_INDEX = 4;
+const CELL_WIDTH = 44;
+const CELL_HEIGTH = 44;
+const SKY_INDEX = 0;
+const WALL_INDEX = 1;
+const BLOCK_INDEX = 2;
+const HOLE_INDEX = 3;
+const GRASS_INDEX = 4;
+const HERO_INDEX = 5;
+
